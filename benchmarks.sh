4895fe7d1a2e5fc72c3771d2cb7aa26593dc0e5c
Yuya Kawabata <yuya.kawabata@techouse.jp>
2024-11-15 11:57:37 +0900

    全言語の結果を CSV 二出力できるようにする

--- benchmarks.sh
+++ benchmarks.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
-# This script is used to run the benchmarks for the project.
+languages=("python" "ruby" "go" "rust" "c")
 
-cd $PWD/c && gcc -O3 -o main main.c && ./main
+echo "Language,Row_Col_Time,Col_Row_Time" > results.csv
 
-cd $PWD/rust && cargo build --release && target/release/main
+base_dir=$(pwd)
 
-cd $PWD/go && go test -bench .
+for lang in "${languages[@]}"
+do
+    case "$lang" in
+        "python")
+            cmd="python3 $base_dir/python/main.py"
+            ;;
+        "ruby")
+            cmd="ruby $base_dir/ruby/main.rb"
+            ;;
+        "go")
+            (cd "$base_dir/go" && go build -o main main.go)
+            cmd="$base_dir/go/main"
+            ;;
+        "rust")
+            (cd "$base_dir/rust" && cargo build --release)
+            cmd="$base_dir/rust/target/release/main"
+            ;;
+        "c")
+            (cd "$base_dir/c" && gcc -O3 -o main main.c)
+            cmd="$base_dir/c/main"
+            ;;
+    esac
 
-cd $PWD/ruby && ruby main.rb
+    output=$($cmd)
 
-cd $PWD/python && python3 main.py
+    # grepとawkで時間を抽出
+    row_col_time=$(echo "$output" | grep 'Average row_col function time' | awk '{print $5}')
+    col_row_time=$(echo "$output" | grep 'Average col_row function time' | awk '{print $5}')
+
+    # 'seconds'を削除
+    row_col_time=${row_col_time% seconds}
+    col_row_time=${col_row_time% seconds}
+
+    # データをCSVファイルに追加
+    echo "$lang,$row_col_time,$col_row_time" >> results.csv
+done
+
+echo "ベンチマーク結果がresults.csvに保存されました"
--- go/main.go
+++ go/main.go
@@ -1,8 +1,39 @@
 package main
 
+import (
+    "fmt"
+    "time"
+)
+
 const (
 	SIZE = 10240
+    ITERATIONS = 5
 )
+
+func main() {
+    a := setup()
+
+    var rowColTotalTime time.Duration
+    for iter := 0; iter < ITERATIONS; iter++ {
+        start := time.Now()
+        rowCol(a)
+        elapsed := time.Since(start)
+        rowColTotalTime += elapsed
+    }
+    rowColAvgTime := rowColTotalTime.Seconds() / float64(ITERATIONS)
+
+    var colRowTotalTime time.Duration
+    for iter := 0; iter < ITERATIONS; iter++ {
+        start := time.Now()
+        colRow(a)
+        elapsed := time.Since(start)
+        colRowTotalTime += elapsed
+    }
+    colRowAvgTime := colRowTotalTime.Seconds() / float64(ITERATIONS)
+
+    fmt.Printf("Average row_col function time: %.5f seconds\n", rowColAvgTime)
+    fmt.Printf("Average col_row function time: %.5f seconds\n", colRowAvgTime)
+}
 
 // 配列の初期化
 func setup() [][]int {
